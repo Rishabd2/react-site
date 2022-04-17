209fbb0ad380e1f2ae472e0812b33463cc056a31
Ravyu Desktop <ravyusk@gmail.com>
2022-04-17 15:32:14 -0500

    Restructured team profile json

--- src/components/TeamProfile.js
+++ src/components/TeamProfile.js
@@ -6,11 +6,6 @@ function TeamProfile(props){
    let profiles = JSON.parse(JSON.stringify(profiles_orig))
    let contents = []
 
-   // Co-Directors on top
-   contents.push(<RoleProfile title={profiles["codirectors"].pretty_name} persons={profiles["codirectors"].persons} />)
-
-   delete profiles["codirectors"]
-
    for(let role in profiles){
        contents.push(<RoleProfile title={profiles[role].pretty_name} persons={profiles[role].persons} />)
    }
